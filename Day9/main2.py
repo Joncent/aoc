ba0f35c1e6312b2769fda4fed83b398c68ab17d7
Joncent <jojo.pfennig@gmail.com>
2024-05-31 11:49:40 +0200

    Second challenge of Day9 done

---
+++ Day9/main2.py
@@ -0,0 +1,55 @@
+import sys
+
+input = open("input.txt")
+lines = input.readlines()
+data = []
+for i in range(len(lines)):
+    lines[i] = lines[i].replace("\n", "")
+    numArray = lines[i].split(" ")
+    for j in range(len(numArray)):
+        numArray[j] = int(numArray[j])
+    data.append(numArray)
+
+
+
+
+#recursive function for finding next number in line
+#function takes array and returns array with appended last digit
+def InterpolateArray(array):
+    
+    if (isZeroArray(array)):
+        array.append(0)
+        return array
+    else:
+        array.append(InterpolateArray(findDiffArray(array))[-1] + array[-1])
+        return array
+
+def InterpolateArrayBackwards(array):
+
+    if (isZeroArray(array)):
+        array.append(0)
+        return array
+    else:
+        array.insert(0, array[0] - InterpolateArrayBackwards(findDiffArray(array))[0])
+        return array
+    
+
+def findDiffArray(array):
+    diffArray = []
+    for i in range(len(array)-1):
+        diff = array[i+1]-array[i]
+        diffArray.append(diff)
+    return diffArray
+
+def isZeroArray(array):
+    for number in array:
+        if number != 0:
+            return False
+    return True
+
+result = 0
+
+for i in range(len(data)):
+    result += InterpolateArrayBackwards(data[i])[0]
+
+print(result)
